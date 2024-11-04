5b5d89809d6f56f938459aa9f23100709e21743e
gaoryrt <gaoryrt@gmail.com>
2024-11-04 16:25:02 +0800

    feat: 🎸 dnd

--- src/List.tsx
+++ src/List.tsx
@@ -1,6 +1,14 @@
 import styles from "./List.module.less";
 import { data, Ranks } from "./env";
 import React, { useState } from "react";
+
+import { ReactSortable } from "react-sortablejs";
+
+interface ItemType {
+  id: number;
+  name: string;
+}
+
 const LIST: { rank: Ranks; color: string }[] = [
   {
     rank: "S",
@@ -29,29 +37,41 @@ const LIST: { rank: Ranks; color: string }[] = [
 ];
 
 export function List() {
-  const [tracks, setTracks] = useState(
-    data.tracks.items.map((o) => {
+  const [tracks, setTracks] = useState({
+    F: data.tracks.items.map((o) => {
       const t = o.track;
       return {
         name: t.name,
         artist: t.artists.map((a) => a.name).join(", "),
         cover: t.album.images[0].url,
-        rank: "S",
       };
-    })
-  );
+    }),
+  });
   return LIST.map((o) => (
     <div className={styles.tierlist} key={o.rank}>
       <div className={styles.title} style={{ background: o.color }}>
         {o.rank}
       </div>
-      <div className={styles.blocklist}>
-        {tracks
-          .filter((i) => o.rank === i.rank)
-          .map((o, index) => (
-            <Card {...o} key={index} />
-          ))}
-      </div>
+      <ReactSortable
+        animation={150}
+        group={{
+          name: "shared",
+          pull: true,
+          put: true,
+        }}
+        list={tracks[o.rank] || []}
+        setList={(newList) => {
+          setTracks((prev) => ({
+            ...prev,
+            [o.rank]: newList,
+          }));
+        }}
+        className={styles.blocklist}
+      >
+        {tracks[o.rank]?.map((o, index) => (
+          <Card {...o} key={index} />
+        ))}
+      </ReactSortable>
     </div>
   ));
 }
